ff8fc01012a8be076a72568faa9a33d7dc627455
Bedirhan Giden <bedirhangiden@Bedirhan-MacBook-Air.local>
2021-12-12 02:00:52 +0300

    Added bodyparser

--- server/app.js
+++ server/app.js
@@ -1,28 +1,29 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv/config");
-const authJwt = require("./helpers/jwt");
-const errorHandler = require("./helpers/error-handler");
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
 
 app.use(cors());
-app.options("*", cors());
+app.options('*', cors());
 
 //middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 //app.use(authJwt());
-app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
 app.use(errorHandler);
 
 //Routes
-const categoriesRoutes = require("./routes/categories");
-const productsRoutes = require("./routes/products");
-const usersRoutes = require("./routes/users");
-const ordersRoutes = require("./routes/orders");
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
 
 const api = process.env.API_URL;
 
@@ -33,19 +34,19 @@ app.use(`${api}/orders`, ordersRoutes);
 
 //Database
 mongoose
-  .connect(process.env.CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: "mean-eshop",
-  })
-  .then(() => {
-    console.log("Database Connection is ready...");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(process.env.CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: 'mean-eshop'
+    })
+    .then(() => {
+        console.log('Database Connection is ready...');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 //Server
 app.listen(3000, () => {
-  console.log("server is running http://localhost:3000");
+    console.log('server is running http://localhost:3000');
 });
