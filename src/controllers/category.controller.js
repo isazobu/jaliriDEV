e2573dfb99b26e6828b5983c9edfd5bed8128d4c
İsa Zobu <isaazobu@gmail.com>
2021-12-27 14:31:15 +0300

    category validation + eslint

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -9,7 +9,7 @@ const createCategory = catchAsync(async (req, res) => {
   res.status(httpStatus.CREATED).send(category);
 });
 
-const getCategories= catchAsync(async (req, res) => {
+const getCategories = catchAsync(async (req, res) => {
   const filter = pick(req.query, ['title', 'isActive']);
   const options = pick(req.query, ['sortBy', 'limit']);
   const result = await categoryService.queryCategories(filter, options);
@@ -17,42 +17,36 @@ const getCategories= catchAsync(async (req, res) => {
 });
 
 const getCategory = catchAsync(async (req, res) => {
-  
-  const category = await categoryService.getCategoryById(req.params.categoryId)
-  if(!category) {
+  const category = await categoryService.getCategoryById(req.params.categoryId);
+  if (!category) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Category not found');
-  }  
+  }
   res.status(httpStatus.OK).send(category);
-})
-
+});
 
 const getCategoryByTitle = catchAsync(async (req, res) => {
-  
-  const category = await categoryService.getCategoryByTitle(req.params.categoryTitle)
-  if(!category) {
+  const category = await categoryService.getCategoryByTitle(req.params.categoryTitle);
+  if (!category) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Category not found');
-  }  
+  }
   res.status(httpStatus.OK).send(category);
-})
-
-
+});
 
 const updateCategory = catchAsync(async (req, res) => {
-  const category = await categoryService.updateCategoryById(req.params.categoryId,req.body)
+  const category = await categoryService.updateCategoryById(req.params.categoryId, req.body);
   res.send(category);
-})
+});
 
 const deleteCategory = catchAsync(async (req, res) => {
   await categoryService.deleteCategoryById(req.params.categoryId);
   res.status(httpStatus.NO_CONTENT).send();
-})
+});
 
 module.exports = {
-    createCategory,
-    getCategories,
-    getCategory,
-    getCategoryByTitle,
-    updateCategory,
-    deleteCategory
-
-} 
+  createCategory,
+  getCategories,
+  getCategory,
+  getCategoryByTitle,
+  updateCategory,
+  deleteCategory,
+};
