31fc5431656fc37839eada45b5c7ab2a876ce5f0
İsa Zobu <isaazobu@gmail.com>
2022-01-06 19:28:22 +0300

    dev: Color e2e dev

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
 const categoryRoute = require('./category.route');
-// const orderRoute = require('./order.route');
+
+const colorRoute = require('./color.route');
+const sizeRoute = require('./size.route');
+const skuRoute = require('./sku.route');
+const countryRoute = require('./country.route');
 const addressRoute = require('./address.route');
 const productRoute = require('./product.route');
 const docsRoute = require('./docs.route');
+// const cartRoute = require('./cart.route');
+// const orderRoute = require('./order.route');
+// const paymentRoute = require('./payment.route');
+// const shippingRoute = require('./shipping.route');
+// const reviewRoute = require('./review.route');
+// const orderDetailRoute = require('./orderDetail.route');
+// const orderStatusRoute = require('./orderStatus.route');
+// const orderHistoryRoute = require('./orderHistory.route');
 const config = require('../../config/config');
 
 const router = express.Router();
@@ -19,18 +31,41 @@ const defaultRoutes = [
     path: '/users',
     route: userRoute,
   },
+
   {
     path: '/categories',
     route: categoryRoute,
   },
+  // TODO: Add swagger and spell check
   {
     path: '/products',
     route: productRoute,
   },
+  // TODO: Add swagger and spell check
   {
     path: '/addresses',
     route: addressRoute,
   },
+  // TODO: Add swagger and spell check
+  {
+    path: '/countries',
+    route: countryRoute,
+  },
+  // TODO: Add swagger and spell check
+  {
+    path: '/colors',
+    route: colorRoute,
+  },
+  // TODO: Add swagger and spell check
+  {
+    path: '/sizes',
+    route: sizeRoute,
+  },
+  // {
+  //   path: 'skus',
+  //   route: skuRoute,
+  // },
+
   // {
   //   path: '/orders',
   //   routes: orderRoute,
