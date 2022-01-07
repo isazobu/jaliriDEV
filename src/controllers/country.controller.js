ad186c0bd9692098e416cf863ad6d8ec42e1d092
İsa Zobu <isaazobu@gmail.com>
2022-01-07 23:45:55 +0300

    remove unused var

--- src/controllers/country.controller.js
+++ src/controllers/country.controller.js
@@ -1,5 +1,5 @@
 const httpStatus = require('http-status');
-const pick = require('../utils/pick');
+
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 const { countryService } = require('../services');
