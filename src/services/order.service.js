47feaad4e1caeae385c6bc8bc88fc6b52c583aeb
zahid-cakici <cakici19@itu.edu.tr>
2022-02-03 14:55:37 +0300

    dev: Order service controller validation added

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,20 +1,56 @@
+/* eslint-disable no-use-before-define */
 const httpStatus = require('http-status');
+const mongoose = require('mongoose');
 const { Order, OrderItem, Address } = require('../models');
+const User = require('../models/user.model');
 const ApiError = require('../utils/ApiError');
+
+// /**
+//  * Create a order
+//  * @param {Object} orderBody
+//  * @returns {Promise<Order>}
+//  */
+// const createOrder = async (orderBody) => {
+//   const { orderItems, address, ...order } = orderBody;
+//   const orderItemsIds = orderItems.map((orderItem) => {
+//     const newOrderItem = this.createOrReadOrderItem(orderItem);
+//     return newOrderItem._id;
+//   });
+//   const addressId = this.createOrReadAdress(address);
+//   return Order.create({ orderItems: orderItemsIds, address: addressId, ...order });
+// };
 
 /**
  * Create a order
+ * @param {ObjectId} userId
  * @param {Object} orderBody
  * @returns {Promise<Order>}
  */
-const createOrder = async (orderBody) => {
-  const { orderItems, address, ...order } = orderBody;
-  const orderItemsIds = orderItems.map((orderItem) => {
-    const newOrderItem = this.createOrReadOrderItem(orderItem);
-    return newOrderItem._id;
+const createOrder = async (userId, orderBody) => {
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'User not found');
+  }
+  const address = await Address.findById(orderBody.address);
+  if (!address || address.user.toString() !== userId.toString()) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Address not found');
+  }
+  const { addressId, ...other } = orderBody;
+
+  // const addressId = this.createOrReadAdress(address);
+
+  const order = new Order({
+    cart: newCart(),
+    address: mongoose.Types.ObjectId(orderBody.addressId),
+    user: mongoose.Types.ObjectId(userId),
+    ...other,
   });
-  const addressId = this.createOrReadAdress(address);
-  return Order.create({ orderItems: orderItemsIds, address: addressId, ...order });
+
+  Object.assign(order.cart, user.cart);
+  user.cart = undefined;
+  await user.save();
+
+  return order.save();
 };
 
 const createOrReadOrderItem = async (orderItemBody) => {
@@ -57,14 +93,10 @@ const getOrderById = async (orderId) => {
  * @param {Object} updateBody
  * @returns {Promise<Order>}
  */
-
 const updateOrderById = async (orderId, updateBody) => {
   const order = await getOrderById(orderId);
   if (!order) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Order not found');
-  }
-  if (updateBody.title && (await Order.isOrderExist(updateBody.title, orderId))) {
-    throw new ApiError(httpStatus.BAD_REQUEST, 'Order already taken');
   }
   Object.assign(order, updateBody);
   await order.save();
@@ -85,6 +117,32 @@ const deleteOrderById = async (orderId) => {
   return order;
 };
 
+/**
+ * Delete order of the user by id
+ * @param {ObjectId} orderId
+ * @returns {Promise<Order>}
+ */
+const deleteMyOrderById = async (orderId, userId) => {
+  const order = await getOrderById(orderId);
+  if (!order) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Order not found');
+  }
+  if (order.user.toString() !== userId.toString()) {
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Unauthorized');
+  }
+  await order.remove();
+  return order;
+};
+
+const newCart = () => {
+  return {
+    cart: [],
+    totalPrice: 0,
+    itemsPrice: 0,
+    shippingPrice: 0,
+  };
+};
+
 module.exports = {
   createOrder,
   queryOrders,
@@ -93,4 +151,5 @@ module.exports = {
   createOrReadOrderItem,
   updateOrderById,
   deleteOrderById,
+  deleteMyOrderById,
 };
