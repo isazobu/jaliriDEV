1c239e5b01c02e685bdd91b566c1f4224b5bae80
İsa Zobu <isaazobu@gmail.com>
2022-02-22 20:14:23 +0300

    dev: country filter param to header

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -2,7 +2,7 @@ const httpStatus = require('http-status');
 const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
-const { productService, attributeService } = require('../services');
+const { productService, countryService } = require('../services');
 
 const createProduct = catchAsync(async (req, res) => {
   const productItem = await productService.createProduct(req.body);
@@ -38,17 +38,18 @@ const getProductBySku = catchAsync(async (req, res) => {
 });
 
 const getProducts = catchAsync(async (req, res) => {
-  const filter = pick(req.query, [
-    'productId',
-    'title',
-    'discountExist',
-    'price',
-    'category',
-    'brand',
-    'size',
-    'color',
-    'country',
-  ]);
+  const filter = pick(req.query, ['productId', 'title', 'discountExist', 'price', 'category', 'brand', 'size', 'color']);
+  // append filter array from header country
+  if (req.headers.country) {
+    const country = await countryService.getCountryByCode(req.headers.country);
+    if (country) {
+      filter.country = req.headers.country;
+    } else {
+      res.status(httpStatus.NOT_FOUND).send('Country not found');
+    }
+  } else {
+    res.status(httpStatus.NOT_FOUND).send('Country not found');
+  }
 
   const options = pick(req.query, ['sortBy', 'limit', 'page']);
 
