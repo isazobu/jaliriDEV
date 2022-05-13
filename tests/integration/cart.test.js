aeec46297f00eefb4a28b4ea18f273f51e189065
zahid-cakici <cakici19@itu.edu.tr>
2022-05-13 22:57:15 +0300

    dev: Cart Integration tests update

--- tests/integration/cart.test.js
+++ tests/integration/cart.test.js
@@ -1,5 +1,4 @@
 const request = require('supertest');
-const faker = require('faker');
 const mongoose = require('mongoose');
 const httpStatus = require('http-status');
 const app = require('../../src/app');
@@ -40,7 +39,7 @@ describe('Cart routes', () => {
         },
       ];
     });
-    test('should return 201 and successfully create new user if data is ok', async () => {
+    test('should return 201 and successfully add to cart if data is ok', async () => {
       await insertUsers([userOne]);
       await insertProducts(productOne);
 
@@ -59,5 +58,101 @@ describe('Cart routes', () => {
       expect(cart).toBeDefined();
       expect(cart).toMatchObject(response.body);
     });
+
+    test('should return 401 if access token is missing', async () => {
+      await request(app).post('/api/v1/carts').send().expect(httpStatus.UNAUTHORIZED);
+    });
+
+    test('should return 400 if data is invalid', async () => {
+      await insertUsers([userOne]);
+      await insertProducts(productOne);
+
+      await request(app)
+        .post('/api/v1/carts')
+        .set('Authorization', `Bearer ${userOneAccessToken}`)
+        .send({ items: [] })
+        .expect(httpStatus.BAD_REQUEST);
+    });
+
+    test('should return 400 if quantity is invalid', async () => {
+      await insertUsers([userOne]);
+      await insertProducts(productOne);
+      items[0].quantity = -1;
+
+      await request(app)
+        .post('/api/v1/carts')
+        .set('Authorization', `Bearer ${userOneAccessToken}`)
+        .send({ items })
+        .expect(httpStatus.BAD_REQUEST);
+    });
+
+    test('should return 404 if variant is not found', async () => {
+      await insertUsers([userOne]);
+      await insertProducts(productOne);
+      items[0].sku = '123';
+
+      await request(app)
+        .post('/api/v1/carts')
+        .set('Authorization', `Bearer ${userOneAccessToken}`)
+        .send({ items })
+        .expect(httpStatus.NOT_FOUND);
+    });
+
+    test('should return 400 if stock is not enough', async () => {
+      await insertUsers([userOne]);
+      await insertProducts(productOne);
+
+      await request(app)
+        .post('/api/v1/carts')
+        .set('Authorization', `Bearer ${userOneAccessToken}`)
+        .send({ items: [{ sku: productOne.variants[0].sku.toString(), quantity: 10000000 }] })
+        .expect(httpStatus.BAD_REQUEST);
+    });
+  });
+
+  describe('POST /api/v1/carts/manipulate', () => {
+    describe('insert', () => {
+      let body;
+
+      beforeEach(() => {
+        body = {
+          action: 'insert',
+          sku: productOne.variants[0].sku.toString(),
+          quantity: 2,
+        };
+      });
+
+      test('should return 200 and successfully add to cart if data is ok', async () => {
+        await insertUsers([userOne]);
+        await insertProducts(productOne);
+
+        const response = await request(app)
+          .post('/api/v1/carts/manipulate')
+          .set('Authorization', `Bearer ${userOneAccessToken}`)
+          .send(body)
+          .expect(httpStatus.OK);
+
+        const { cart } = await User.findById(userOne._id).select('cart').lean();
+        expect(cart).toBeDefined();
+        expect(cart).toMatchObject(response.body);
+      });
+
+      test('should return 401 if access token is missing', async () => {
+        await request(app).post('/api/v1/carts/manipulate').send().expect(httpStatus.UNAUTHORIZED);
+      });
+
+      test('should return 400 if data is invalid', async () => {
+        await insertUsers([userOne]);
+        await insertProducts(productOne);
+
+        await request(app)
+          .post('/api/v1/carts/manipulate')
+          .set('Authorization', `Bearer ${userOneAccessToken}`)
+          .send({ action: 'insert' })
+          .expect(httpStatus.BAD_REQUEST);
+      });
+    });
+    describe('delete', () => {});
+    describe('truncate', () => {});
   });
 });
