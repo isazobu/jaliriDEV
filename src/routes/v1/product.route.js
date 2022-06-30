e56765c447b8e664733e54bcedc3035ac28789e9
Ammar Abdulbaki <ammarabdolbaqi4@gmail.com>
2022-07-01 00:43:53 +0300

    add /:productId/statistics route

--- src/routes/v1/product.route.js
+++ src/routes/v1/product.route.js
@@ -4,7 +4,7 @@ const auth = require('../../middlewares/auth');
 // const validate = require('../../middlewares/validate');
 // const productValidation = require('../../validations/product.validation');
 const productController = require('../../controllers/product.controller');
-const { get } = require('../../models/schemas/orderCart.schema');
+// const { get } = require('../../models/schemas/orderCart.schema');
 
 const router = express.Router();
 
@@ -19,6 +19,8 @@ router.route('/many').post(auth('manageProducts'), productController.createManyP
 router.route('/skus').post(productController.getManySku);
 
 router.route('/filters').get(productController.filterProductMenu);
+
+router.route('/:productId/statistics').get(productController.getProductSales);
 
 router
   .route('/:productId')
